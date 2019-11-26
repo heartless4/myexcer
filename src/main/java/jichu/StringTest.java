815a555c6398fe3a8f0a91c94db4da6cd635f162
haojiuhong <haojiuhong@215>
2019-11-26 17:30:13 +0800

    æ·»å ä¿®æ¹

--- src/main/java/jichu/StringTest.java
+++ src/main/java/jichu/StringTest.java
@@ -10,8 +10,18 @@ package jichu;
 public class StringTest {
     public static void main(String args[]){
         String s1=new StringBuilder("go").append("od").toString();
-        System.out.println(s1.intern()==s1);
+        System.out.println(s1.intern()==s1);//true
         String s2=new StringBuilder("ja").append("va").toString();
-        System.out.println(s2.intern()==s2);
+        System.out.println(s2.intern()==s2);//1.8.0_172:false.
+        String str1=new String("abc");
+        String str2=new String("abc");
+        System.out.println(str1.intern()==str1);//false
+        System.out.println(str2.intern()==str2);//false
+        System.out.println(str1.intern()==str2.intern());//true
+        String string2=new String("string")+new String("01");
+        string2.intern();
+        String string1="string01";
+        System.out.println(string2==string1);//true
+
     }
 }
