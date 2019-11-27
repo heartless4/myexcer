6ebca3034f24c6088fdd00cade530b757a1bce96
heartless4 <626584609@qq.com>
2019-11-27 15:05:59 +0800

    测试冲突提交

--- src/main/java/jichu/StringTest.java
+++ src/main/java/jichu/StringTest.java
@@ -10,8 +10,10 @@ package jichu;
 public class StringTest {
     public static void main(String args[]){
         String s1=new StringBuilder("go").append("od").toString();
-        System.out.println(s1.intern()==s1);
+        System.out.println(s1.intern()==s1); //true
         String s2=new StringBuilder("ja").append("va").toString();
-        System.out.println(s2.intern()==s2);
+        System.out.println(s2.intern()==s2);//false
+       /* String s3=new StringBuilder("ni").append("hao").toString();
+        System.out.println(s3.intern()==s3);//false*/
     }
 }
